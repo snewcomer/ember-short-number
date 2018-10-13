37a54fcea915f6df89ad48eb7c24ead8d792e61b
snewcomer <snewcomer24@gmail.com>
2018-10-13 10:42:33 +0200

    Basic extraction of all locale data with number fields

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const extract = require('broccoli-cldr-data');
+const mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  treeForApp(tree) {
+    let trees = [tree];
+
+    if (tree && this.locales.length) {
+      let cldrTree = extract(tree, {
+        numberFields: true,
+        destDir: 'cldrs-short',
+        prelude: '/*jslint eqeq: true*/\n',
+        moduleType: 'es6'
+      });
+
+      trees.push(cldrTree);
+    }
+
+    return mergeTrees(trees, { overwrite: true });
+  }
 };
