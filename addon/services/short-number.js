296f53366019444d7a0818a72f7eb8198766e446
Scott Newcomer <snewcomer@users.noreply.github.com>
2018-11-04 09:06:35 -0800

    Move format logic to own npm package (#15)

--- addon/services/short-number.js
+++ addon/services/short-number.js
@@ -1,9 +1,11 @@
 import { getOwner } from '@ember/application';
 import { set } from '@ember/object';
+import { assign } from '@ember/polyfills';
 import Service from '@ember/service';
 import hydrate from '../-private/hydrate';
-import { extractIntPart, isLessThanBoundary, normalizeNumber } from '../-private/math-utils';
-import { findLocaleData, needsFormatting, normalizeLocal, replaceNumber } from '../-private/utils';
+import cldrCompactNumber from 'cldr-compact-number';
+
+const { compactFormat } = cldrCompactNumber;
 
 export default Service.extend({
   __localeData__: null,
@@ -64,79 +66,8 @@ export default Service.extend({
    * @return {String}
    */
   format(value, locale = 'en', digitsConfig = {}) {
-    // coerce to number
-    let number = Number(value);
-    if (!value || typeof number !== 'number') {
-      return value;
-    }
-
-    // figure out which numbers hash based on the locale
-    locale = normalizeLocal(locale); // en_GB -> en-GB
-    let localeData = findLocaleData(this.__localeData__, locale);
-    if (!localeData) {
-      return value;
-    }
-
-    // take the absolute value and stash sign to apply at end
-    let sign = 1;
-    if (number < 0) {
-      sign = -1;
-      number = Math.abs(number);
-    }
-
-    // find specific rules: short or long
-    let { financialFormat = false, long = false } = digitsConfig;
-    let rules = long ? localeData.decimal.long : localeData.decimal.short;
-    if (!rules || number < 1000) {
-      return value;
-    }
-
-    // 1. Take number and determine range it is in
-    // 2. Extract specific rule from hash - ["0K", 1] meaning which value from the rule and number of zeros
-    let matchingRule;
-    let arbitraryPrecision = 0;
-    for (let i = 0; i <= rules.length; i++) {
-      if (isLessThanBoundary(number, rules[i][0])) {
-
-        let [testRangeHigh] = rules[i];
-        // always use previous rule until within 5% threshold of upper limit
-        if (!financialFormat && (1 - (number / testRangeHigh) > this.threshold)) {
-          // e.g use 950K instead of 1M
-          // e.g use 101K instead of 0.1M
-          matchingRule = rules[i - 1];
-        } else {
-          matchingRule = rules[i];
-          if (!digitsConfig.significantDigits || !financialFormat) {
-            // if we want to round up, we need to prevent numbers like 99,499 from rounding down to 99K
-            // /-private/math-utils will use this variable to round a number like 91 to 100 since we are within the threshold
-            arbitraryPrecision = 1;
-          }
-        }
-        break;
-      }
-    }
-
-    // 3. Normalise number by converting to decimal and cropping to number of digits
-    //  1000 -> 1.000 -> 1K
-    //  1600 -> 1.600 -> 2K
-    // 4. Format according to formatter e.g. "0K"
-    let [range, opts] = matchingRule;
-    // cldr data is either `one` or `other`.  Defaulting to `one` for now
-    let [format, numberOfDigits] = opts.one || opts.other;
-
-    if (!needsFormatting(format)) {
-      return value;
-    }
-
-    let normalized = normalizeNumber(
-      extractIntPart(number, range, numberOfDigits),
-      arbitraryPrecision,
-      sign,
-      locale,
-      digitsConfig
-    );
-
-    return replaceNumber(normalized, format);
+    const options = assign({}, { threshold: this.threshold }, digitsConfig);
+    return compactFormat(value, locale, this.__localeData__, options)
   }
 });
 
